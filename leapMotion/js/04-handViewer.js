69c3826dbc3bd9a56c518c3a4fb1e77542c7c2a3
Joshua Stopper <jmstopper@hotmail.com>
2013-08-27 13:24:00 +1000

    Updated hand viewer

--- leapMotion/js/04-handViewer.js
+++ leapMotion/js/04-handViewer.js
@@ -8,25 +8,31 @@ function frameController(frame){
 	if(frame.hands.length<1){
 		//Do nothing
 	}else{
-		for(var i=0;i<frame.hands.length;i++){
 
-			var palmX = normalised(frame.hands[i].palmPosition[0])
-			var palmY = normalised(frame.hands[i].palmPosition[1])
+		if(frame.pointables.length>1){
 
-			for(var x=0;x<frame.hands[i].pointables.length;x++){
-				
-				var tipX = normalised(frame.hands[i].pointables[x].tipPosition[0])
-				var tipY = normalised(frame.hands[i].pointables[x].tipPosition[1])
+			for(var i=0;i<frame.hands.length;i++){
+				$("#coordinates").html(frame.hands[0].palmPosition[0]+"<br>"); //Left to right or X
+				$("#coordinates").append(frame.hands[0].palmPosition[1]+"<br>"); //Up and down Z
+				$("#coordinates").append(frame.hands[0].palmPosition[2]+"<br>"); //Y Back and forth
+				var palmX = normalised(frame.hands[i].palmPosition[0])
+				var palmY = normalised(frame.hands[i].palmPosition[2])
 
-				context.beginPath();
-				context.moveTo(palmX, palmY);
-				context.lineTo(tipX, tipY);
-				context.stroke();
+				for(var x=0;x<frame.hands[i].pointables.length;x++){
+					
+					var tipX = normalised(frame.hands[i].pointables[x].tipPosition[0])
+					var tipY = normalised(frame.hands[i].pointables[x].tipPosition[2])
+
+					context.beginPath();
+					context.moveTo(palmX, palmY);
+					context.lineTo(tipX, tipY);
+					context.stroke();
+				}
 			}
 		}
 	}
 	
-	//$("#coordinates").html(frame.dump());
+	
 }
 
 function normalised(value){

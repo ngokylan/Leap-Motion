f05760de9227d71fa57103ede9aeeb9c52936a32
ngokylan <ngokylan@gmail.com>
2013-10-08 11:57:15 +1100

    Eliminate unnecessary files

--- leapMotion/version2/jsg/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-	$("#side-nav li").mouseover(function(){
-		$(this).addClass("active").siblings(".active").removeClass("active");
-		var referenceId = $(this).data("reference");
-		$("#"+referenceId).addClass("active").siblings(".reference").removeClass("active");
-	});
-});

8c127edd2627d55cbc4ae6ac0b3b55038c5f0bd6
Joshua Stopper <jmstopper@hotmail.com>
2013-11-05 18:06:51 +1100

    Updated and included the work contribution file

--- leapMotion/version3/js/LeapMotion/lmController.js
+++ leapMotion/version3/js/LeapMotion/lmController.js
@@ -22,17 +22,17 @@ $(document).ready(function() {
 
 //This event fires when a successful connection has been made with the lmDevice
 controller.on('connect', function() {
-	//baseInfo("Successfully connected.");
+	
 });
 
 //This is fired when the script is already running and the lmDevice is then connected
 controller.on('deviceConnected', function() {
-	//baseInfo("A Leap device has been connected.");
+	
 });
 
 //This is fired when the script is running and the lmDevice is disconnected
 controller.on('deviceDisconnected', function() {
-	//baseInfo("A Leap device has been disconnected.");
+	
 });
 
 //This is fired at 60 frames per second no matter what
